d04622fea4871a3e416e7614d97e693c6bcd136c
Monta-sys <72995093+Monta-sys@users.noreply.github.com>
2020-11-01 02:46:54 +0100

    Update PScan.py

--- PScan.py
+++ PScan.py
@@ -131,7 +131,7 @@ def check(remoteServerIP,t_out,val_L,val_H):
                 result = sock.connect_ex((remoteServerIP, port))
                 if result == 0:
                     print "\n"
-                    print "\033[32m[ {} ] [++] {} is Open\033[97m".format(datetime.now().strftime("%H:%M:%S"),port)
+                    print "\033[32m[ {} ] [++] {}/{} is Open\033[97m".format(datetime.now().strftime("%H:%M:%S"),port,socket.getservbyport(port)) 
                     row=[port]
                     tab.add_row(row)
                     print "\n"
