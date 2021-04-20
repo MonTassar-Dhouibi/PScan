07ae24a99d788506ef7198d3068a2ec9ea51da65
Monta-sys <72995093+Monta-sys@users.noreply.github.com>
2021-04-20 21:36:13 +0100

    Add files via upload

--- PScan.py
+++ PScan.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
-## Script have two attack panel finder and port scanner
+## Script have two attack [`panel finder || port scanner`]
 import socket
-import httplib
+import httplib2 as httplib
 import subprocess
 import sys
 import time
+import texttable
 
-
-
-
-from texttable.texttable import Texttable
+from texttable import Texttable
 from datetime import datetime
 
 subprocess.call('clear', shell=True)
@@ -23,8 +21,8 @@ ______  _____                  ___  ___          _____
 \_|    \____/ \___\__,_|_| |_| \_|  |_/\___/|_| |_\_/\__,_|
                                                            
 _________[+] PScan [+]_[+] V1.01 [+]_[+] By MonTa [+]_______\033[97m"""
-print "{:^}".format(msg)
-print "\x1b[49m" 
+print("{:^}".format(msg))
+print("\x1b[49m")
 
 msg3="""\033[97m\n PScan  -[ARG] [TARGET SITE] -[ATTACK] --[OPTION]\n\n[+]ARGUMENTS:\n\t-v --victimHost   : give the url adress of the target host\n\n[+]ATTACK:\n\t-a --adminFinder  :select panel finder attack \n\t-p --PortScan     : select Ports Scanner attack\n\n[+]OPTION:\n\t-t --Time_Out     :give the value of timeout in secends\n\t-i --interval     :give the interval of ports begin and end to try \n\nExemple:  PScan -v www.hostname.com -a\n\t  PScan -v www.targetHost.com -p -i 23 84 -t 0.5\n\n\t\033[96m   credit by MonTassar_Dhouibi\n\tuploaded in www.github.com/Monta_0\033[97m"""
 
@@ -33,11 +31,11 @@ msg3="""\033[97m\n PScan  -[ARG] [TARGET SITE] -[ATTACK] --[OPTION]\n\n[+]ARGUME
 l=sys.argv
 del l[0]
 if len(l)==0:
-        print "{}".format(msg3)
+        print("{}".format(msg3))
         exit()
 if l[0]=='-h':
-	print "{:=^48}==".format('[*] help [*]')
-	print msg3
+	print("{:=^48}==".format('[*] help [*]'))
+	print(msg3)
 	exit()
 site='';i,time_out,VL,VH=0,0,0,0;a,p=False,False
 type='all'
@@ -45,11 +43,11 @@ i=0
 if l[i]=='-v' and len(l)>1:
 	site =l.pop(i+1);del l[i]
 else:
-	print msg3
+	print(msg3)
 	exit()
 if len(l)==0:
-	print 'no such attack selected'
-	print msg3
+	print('no such attack selected')
+	print(msg3)
 	exit()
 if l[i]=='-p':
 	p=True
@@ -62,7 +60,7 @@ if l[i]=='-p':
 				time_out=float(l.pop(i+1))
 				del l[i]
 			else:
-				print msg3
+				print(msg3)
 				exit()
 elif l[i]=='-a':
 	a=True;del l[i]
@@ -72,31 +70,36 @@ elif l[i]=='-a':
 			type=type.replace("-","")
 			del l[i]
 		else :
-			print "\n\033[91m[!] source code type error -s [SOURCE TYPE]  [!]\033[97m\n"
-			print msg3
+			print("\n\033[91m[!] source code type error -s [SOURCE TYPE]  [!]\033[97m\n")
+			print(msg3)
 			exit()
 else:
-	print 'no such attack selected'
-	print msg3
+	print('no such attack selected')
+	print(msg3)
 	exit()
                 
 
 
+table=True
+try :
+	tab = Texttable()
+	header = ['Ports']
+	tab.header(header)
+	tab.set_deco(tab.HEADER |tab.		BORDER)
+	tab.set_cols_width([8])
+	tab.set_cols_align(["c"])
+	tab.set_cols_dtype(['a']) # automatic
+except :
+	table = False
+	input("[\] error with text table press any key to continue without tables")
 
-tab = Texttable()
-header = ['Ports']
-tab.header(header)
-tab.set_deco(tab.HEADER |tab.BORDER)
-tab.set_cols_width([8])
-tab.set_cols_align(["c"])
-tab.set_cols_dtype(['a']) # automatic
 
 try:
     site = site.replace("http://","")
     print ("\tChecking website " + site + "...")
     conn = httplib.HTTPConnection(site)
     conn.connect()
-    print "\t\033[32m[$] Yes... Server is Online.\033[97m"
+    print("\t\033[32m[$] Yes... Server is Online.\033[97m")
     RSIP  = socket.gethostbyname(site)
 except (httplib.HTTPResponse, socket.error) as Exit:
     raw_input("\t \033[91m[!] Oops Error occured, Server offline or invalid URL\033[97m")
@@ -111,56 +114,49 @@ def check(remoteServerIP,t_out,val_L,val_H):
             t1 = datetime.now()
             varP=""
             for port in range(val_L,val_H+1):
-                print "\033[94m[",datetime.now().strftime("%H:%M:%S"),"]","\033[97m[#] cheaking... ",remoteServerIP,":",port
+                print("\033[94m[",datetime.now().strftime("%H:%M:%S"),"]","\033[97m[#] cheaking... ",remoteServerIP,":",port)
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 sock.settimeout(t_out)
                 result = sock.connect_ex((remoteServerIP, port))
                 if result == 0:
-                    print "\n"
-                    print "\033[32m[ {} ] [++] {}/{} is Open\033[97m".format(datetime.now().strftime("%H:%M:%S"),port,socket.getservbyport(port))
+                    print("\n")
+                    print("\033[32m[ {} ] [++] {}/{} is Open\033[97m".format(datetime.now().strftime("%H:%M:%S"),port,socket.getservbyport(port)))
                     varP+= str(port)+"/"+socket.getservbyport(port) + "   "
-                    
-                    row=[str(port)+"/"+socket.getservbyport(port)] 
-                    tab.add_row(row)
-                    print "\n"
+                    if table: 
+                   	 row=[str(port)+"/"+socket.getservbyport(port)] 
+                   	 tab.add_row(row)
+                    print("\n")
                     sock.close()
         except KeyboardInterrupt:
-        	print "\n\t[!]You pressed Ctrl+C"
-        	print "\t[!]Session cancelled..."
+        	print("\n\t[!]You pressed Ctrl+C")
+        	print("\t[!]Session cancelled...")
         except socket.gaierror:
-            print 'Hostname could not be resolved. Exiting'
+            print('Hostname could not be resolved. Exiting')
             sys.exit()
 
         except socket.error:
-            print "Couldn't connect to server"
+            print("Couldn't connect to server")
             sys.exit()
         
         
         
         t2 = datetime.now()
-
-
-	total =  t2 - t1
-
-
-
-
-	
-        print('{}'.format(tab.draw()))
-	
-	print varP
-		
-
-	print '\tScanning Completed in:   {:7.7}'.format(total)
+        total =  t2 - t1
+        
+        if table :
+            print('{}'.format(tab.draw()))
+        else :
+            print(varP)
+        print('\tScanning Completed in:   {:7.7}'.format(total))
 
 
 
 
     
 if p :
-    print "-" * 40
-    print "Scanning remote host\033[95m", RSIP
-    print "\033[97m-" * 40
+    print("-" * 40)
+    print("Scanning remote host\033[95m", RSIP)
+    print("\033[97m-" * 40)
 
     
     if time_out==0:
@@ -211,29 +207,29 @@ if a :
                 if response.status == 200:
                         var1 = var1 + 1
                         varL+= host +"   "
-                        print "%s %s" % ( "\n\n\033[32m[+]\033[97m " + host, "\033[32mAdmin page found!\033[97m")
-                        raw_input("Press enter to continue scanning.\n")
+                        print("%s %s" % ( "\n\n\033[32m[+]\033[97m " + host, "\033[32mAdmin page found!\033[97m"))
+                        input("Press enter to continue scanning.\n")
                 elif response.status == 404:
                         var2 = var2
                 elif response.status == 302:
-                        print "%s %s" % ("\n>>>" + host, "Possible admin page (302 - Redirect)")
+                        print("%s %s" % ("\n>>>" + host, "Possible admin page (302 - Redirect)"))
                 else:
-                        print "%s %s %s" % (host, " Interesting response:", response.status)
+                        print("%s %s %s" % (host, " Interesting response:", response.status))
                 connection.close()
             print("\n\nCompleted \n")
-            print var1, " Admin pages found"
-            print var2, " total pages scanned"
-            raw_input("[/] Press Enter to Exit")
+            print(var1, " Admin pages found")
+            print(var2, " total pages scanned")
+            input("[/] Press Enter to Exit")
         except (httplib.HTTPResponse, socket.error):
-            print "\n\t[!] Session Cancelled; Error occured. Check internet settings"
+            print("\n\t[!] Session Cancelled; Error occured. Check internet settings")
         except (KeyboardInterrupt, SystemExit):
-            print "\n\t[!] Session cancelled"
+            print("\n\t[!] Session cancelled")
 
 
         t2 = datetime.now()
         total =  t2 - t1
 
-        print "\n\033[32m[+] {}\033[97m".format(varL)
-        print '\tScanning Completed in:    {:7.7}'.format(total)
+        print("\n\033[32m[+] {}\033[97m".format(varL))
+        print('\tScanning Completed in:    {:7.7}'.format(total))
 
 
